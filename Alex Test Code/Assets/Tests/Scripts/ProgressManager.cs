92c4b635db7720c28002813c6be84336ff716eee
matthew <76193336+mattgilldev@users.noreply.github.com>
2021-04-06 21:48:37 -0400

    first push with project dev work

---
+++ Alex Test Code/Assets/Tests/Scripts/ProgressManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// script that manages the overall experience
+/// </summary>
+public class ProgressManager : MonoBehaviour
+{
+    public GameObject stateholder;
+    public StateClass[] states = null;
+    public StateClass currentState;
+    public StateClass lastState;
+    public int stateindex = 0;
+
+    public bool startexperience = false;
+
+    public int numberofstates = 0;
+
+    private void Start()
+    {
+        stateindex = 0;
+    }
+
+    public void Update()
+    {
+        //only temporary for now
+        if (startexperience)
+        {
+            startexperience = false;
+            StartExperience();
+        }
+    }
+
+    public void StartExperience()
+    {
+        currentState = states[stateindex];
+        TriggerStartingEvents();
+
+        EnableNextstateObjects();
+    }
+
+    public void NextState()
+    {
+        Debug.Log("starting next state!");
+        DeactivateLastState();
+        UpdateStateinfo();
+        TriggerStartingEvents();
+        EnableNextstateObjects();
+    }
+
+    public void UpdateStateinfo()
+    {
+        stateindex++;
+        if (stateindex <= states.Length -1)
+        {
+            lastState = currentState;
+            currentState = states[stateindex];
+            return;
+        }
+
+        if(stateindex > states.Length -1)
+        {
+            Debug.Log("end of experience! Out of states");
+        }
+    }
+
+    public void DeactivateLastState()
+    {
+        Debug.Log("Deactiving old things");
+        foreach (GameObject thing in currentState.Gobjs_turnon)
+        {
+            thing.SetActive(false);
+        }
+
+    }
+
+    public void EnableNextstateObjects()
+    {
+        Debug.Log("Enabling new things");
+        foreach(GameObject thing in currentState.Gobjs_turnon)
+        {
+            thing.SetActive(true);
+        }
+    }
+
+    public void TriggerStartingEvents()
+    {
+        currentState.Events_on_start.Invoke();
+    }
+
+
+    //in edit mode... live update stateorder
+    private void OnValidate()
+    {
+        if (states != null)
+        {
+            states = stateholder.GetComponentsInChildren<StateClass>();
+        }
+
+        if(states == null)
+        {
+            states = null;
+        }
+    }
+}
---
+++ Alex Test Code/Assets/Tests/Scripts/StateClass.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+/// <summary>
+/// Script is only a class for a state for the "ProgressManager.cs" script....
+/// 
+/// To use... make an empty gameobject called "stateholder" to act as the parent... then make empty gameobjects as children... each with this script for each state
+/// "ProgressManager.cs" will grab all of these states and then add them in the editor live and at run time so you can just rearrage the order of the children gameobjects to change the order of the states
+/// </summary>
+public class StateClass : MonoBehaviour
+{
+    public string statename = "";
+    [Tooltip("List of gameobjects you want to turn on at the start of the state")]
+    public List<GameObject> Gobjs_turnon = new List<GameObject>();
+    [Tooltip("There is or isn't an aduio clip to play")]
+    public bool _audioclip = false;
+    [Tooltip("what is the starting point of audio instructions from the clip")]
+    public float audioclip_starttime = 0f;
+    [Tooltip("what is the end point of audio instructions from the clip")]
+    public float audioclip_endtime = 0f;
+    [Tooltip("There is no task for the user in this step, only playing the audio for instruction...so if this is true, then once the audio is done playing, go to the next state.,. false = there is a task the user needs to do")]
+    public bool _Notasktodo = false;
+    [Tooltip("anything to trigger on start of the state")]
+    public UnityEvent Events_on_start;
+    [Tooltip("anything to trigger on end (completing the state")]
+    public UnityEvent Events_on_end;
+
+    [Header("for testing only")]
+    public bool endstate = false;
+    //runs in edit mode to live update the name of the gameobject
+    private void OnValidate()
+    {
+        statename = gameObject.name;
+    }
+
+
+    public void EndThisState()
+    {
+        Events_on_end.Invoke();
+    }
+
+
+    private void Update()
+    {
+        if (endstate)
+        {
+            endstate = false;
+            EndThisState();
+        }
+    }
+
+}
