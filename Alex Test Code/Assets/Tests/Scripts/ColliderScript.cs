94a0f6670cac1248161cb57127d3609acef91489
aguardo1 <79178045+aguardo1@users.noreply.github.com>
2021-04-13 14:43:13 -0400

    Collider Script Edit
    Added Collider Script

---
+++ Alex Test Code/Assets/Tests/Scripts/ColliderScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour
+{
+    public bool DiaphragmEntered = false;
+    public int Time = 0;
+
+    public bool Waited = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Diaphragm")
+        {
+            DiaphragmEntered = true;
+           StartCoroutine(CountToFive());
+            Time = 0;
+        }
+
+    }
+    private void Update()
+    {
+        if (Waited == true && Time < 5)
+        {
+            Time += 1;
+            Waited = false;
+            StartCoroutine(CountToFive());
+        }
+
+        if (Time == 5)
+        {
+            OnTimeMet();
+        }
+    }
+
+    public void OnTimeMet()
+    {
+        //deactivate gameobject
+        Debug.Log("Timer Complete");
+    }
+
+    IEnumerator CountToFive()
+    {
+        yield return new WaitForSeconds(1);
+        Waited = true;
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Diaphragm")
+        {
+            DiaphragmEntered = false;
+            StopAllCoroutines();
+            Time = 0;
+            Waited = false;
+
+        }
+    }
+}
