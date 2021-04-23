3a995d6e0faffe65c095e72ced5dbb5e6937256e
aguardo1 <79178045+aguardo1@users.noreply.github.com>
2021-04-23 18:57:35 -0400

    New Trigger Script
    Script created for end states of approaching patient steps

---
+++ Alex Test Code/Assets/Tests/Scripts/TriggerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    public StateClass ColliderState;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            this.gameObject.SetActive(false);
+            ColliderState.EndThisState();
+        }
+    }
+}
