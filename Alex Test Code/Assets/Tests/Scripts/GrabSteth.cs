fb01c1f21e26f1d57046d5b906c63b8513218839
aguardo1 <79178045+aguardo1@users.noreply.github.com>
2021-05-08 13:05:26 -0400

    GrabSteth script

---
+++ Alex Test Code/Assets/Tests/Scripts/GrabSteth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabSteth : MonoBehaviour
+{
+    public StateClass myclasstoend;
+    private bool once = false;
+   
+    public void FirstStethGrab()
+    {
+        if (once == false)
+        {
+            once = true;
+            myclasstoend.EndThisState();
+        }
+    }
+}
